c3dabc645cd172a63c5bb974a6f4bb48dddc522a
Phoenix <117663444@qq.com>
2017-10-27 10:56:32 +0800

    在表单元素事件处理上应用 Throttle 或 Debounce

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
 
 class HelloForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.log = debounce((event) => {
+      console.log(event.target.value)
+    }, 1000)
+
+  }
+
   state = {
     text: ''
   }
 
   handleChange = (event) => {
+    event.persist()
+
     this.setState({
       text: event.target.value
     })
-    console.log('输入：', event.target.value)
+
+    this.log(event)
   }
+
 
   handleSubmit = (event) => {
     event.preventDefault()
